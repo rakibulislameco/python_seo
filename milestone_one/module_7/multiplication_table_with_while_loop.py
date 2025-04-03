3b09cac2c858c2293695f73fa262609339d8c227
rakibulislameco <rakibulislameco@outlook.com>
2025-04-03 18:10:35 +0600

    multiplicatin with While Loop

---
+++ milestone_one/module_7/multiplication_table_with_while_loop.py
@@ -0,0 +1,7 @@
+number = int(input('Enter number: '))
+multiplier = 1
+
+while multiplier <=10:
+    result = number * multiplier
+    print(f'{number} * {multiplier} = {result}')
+    multiplier = multiplier +1
